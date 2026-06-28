f7532302dcd473bb3a0625eb7106e83fd02f09dc
HeLoo17 <111735058+HeLoo17@users.noreply.github.com>
2026-06-28 19:55:08 +0800

    created tiemline for system status display

--- frontend/src/components/pages/SystemStatus.tsx
+++ frontend/src/components/pages/SystemStatus.tsx
@@ -1,18 +1,20 @@
 import { useOutletContext } from "react-router-dom";
 import ConnectionStatusCard from "../displays/connectionStatusCard/ConnectionStatusCard";
 import type { UseSocketReturn, SystemStatus } from "../../hooks/useSocket";
+import SystemEventLog from "../displays/SystemEventLog";
 
 
 
 export default function SystemStatus() {
     const { rawData } = useOutletContext<{ rawData: UseSocketReturn }>();
-    const { systemStatus: rawStatus } = rawData;
+    const { systemStatus: rawStatus, allEvents } = rawData;
 
     return (
         <div className="h-full w-full flex flex-col gap-12">
             <div className="h-fit w-full flex gap-8">
                 <ConnectionStatusCard systemStatus={rawStatus} />
             </div>
+            <SystemEventLog systemStatus={rawStatus} allSocketEvents={allEvents} />
         </div>
     )
 }
